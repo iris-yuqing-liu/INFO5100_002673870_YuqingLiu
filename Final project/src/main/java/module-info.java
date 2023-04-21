d85f9ebd9f8748c93e8941509594ee6c8d5db03d
iris-yuqing-liu <97702076+iris-yuqing-liu@users.noreply.github.com>
2023-04-20 21:20:14 -0700

    Delete Final project directory

--- Final project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.multiformatimagemanagementtool {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.multiformatimagemanagementtool to javafx.fxml;
-    exports com.example.multiformatimagemanagementtool;
-}
